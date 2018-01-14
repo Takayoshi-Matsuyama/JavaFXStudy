c04b0c0d1168f92dff3adf5f6c1eee043af6f808
Takayoshi <tkys0515@gmail.com>
2018-01-15 04:40:49 +0900

    Click event handler

--- src/tech/tkys/java/fx/study/main/Controller.java
+++ src/tech/tkys/java/fx/study/main/Controller.java
@@ -1,4 +1,12 @@
 package tech.tkys.java.fx.study.main;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+    @FXML
+    public void onSelectButtionClicked(ActionEvent event) {
+        System.out.println("onSelectButtionClicked");
+    }
+
 }
