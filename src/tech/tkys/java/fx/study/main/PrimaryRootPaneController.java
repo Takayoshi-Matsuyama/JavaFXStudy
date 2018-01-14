51eb816c73357251ffadc67be22ca4418c982b24
Takayoshi <tkys0515@gmail.com>
2018-01-15 04:48:38 +0900

    Renamed class & resource names to "PrimaryRootPane"

--- src/tech/tkys/java/fx/study/main/PrimaryRootPaneController.java
+++ src/tech/tkys/java/fx/study/main/PrimaryRootPaneController.java
@@ -3,7 +3,7 @@ package tech.tkys.java.fx.study.main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class Controller {
+public class PrimaryRootPaneController {
     @FXML
     public void onSelectButtionClicked(ActionEvent event) {
         System.out.println("onSelectButtionClicked");
